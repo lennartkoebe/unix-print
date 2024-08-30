37cc285a2c3c06944a920a2d2a8dd6eec3a73e10
Lennart Koebe <koebelennart@gmail.com>
2024-08-30 23:09:09 +0200

    chore: apply prettier on remaining ts files

--- src/utils/exec-async.ts
+++ src/utils/exec-async.ts
@@ -4,22 +4,26 @@ import { ExecResponse } from "../types";
 import { exec } from "child_process";
 
 export default function execAsync(cmd: string): Promise<ExecResponse> {
-    return new Promise((resolve, reject) => {
-        exec(cmd, {
-            // The output from lp and lpstat is parsed assuming the language is English.
-            // LANG=C sets the language and the SOFTWARE variable is necessary
-            // on MacOS due to a detail in Apple's CUPS implementation
-            // (see https://unix.stackexchange.com/a/33836)
-            env: {
-                SOFTWARE: "",
-                LANG: "C"
-            }
-        }, (err, stdout, stderr) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({stdout, stderr});
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    exec(
+      cmd,
+      {
+        // The output from lp and lpstat is parsed assuming the language is English.
+        // LANG=C sets the language and the SOFTWARE variable is necessary
+        // on MacOS due to a detail in Apple's CUPS implementation
+        // (see https://unix.stackexchange.com/a/33836)
+        env: {
+          SOFTWARE: "",
+          LANG: "C",
+        },
+      },
+      (err, stdout, stderr) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ stdout, stderr });
+        }
+      }
+    );
+  });
 }
--- src/utils/parse-response.spec.ts
+++ src/utils/parse-response.spec.ts
@@ -1,34 +1,40 @@
-import execAsync from '../utils/exec-async';
-import { getRequestId, default as isPrintComplete } from './parse-response';
+import execAsync from "../utils/exec-async";
+import { getRequestId, default as isPrintComplete } from "./parse-response";
 
-jest.mock('../utils/exec-async');
-jest.mock('../get-default-printer/get-default-printer');
+jest.mock("../utils/exec-async");
+jest.mock("../get-default-printer/get-default-printer");
 
 const queuedStdout = `lp0-39   username             15360   Mon 12 Jun 2023 21:09:48`;
 
-describe('getRequestId', () => {
-  it('returns the job id', async () => {
-    const response = { stdout: 'request id is myDummyPrinter-15 (1 file(s))', stderr: null };
-    const expected = 'myDummyPrinter-15';
+describe("getRequestId", () => {
+  it("returns the job id", async () => {
+    const response = {
+      stdout: "request id is myDummyPrinter-15 (1 file(s))",
+      stderr: null,
+    };
+    const expected = "myDummyPrinter-15";
 
     expect(getRequestId(response)).toEqual(expected);
   });
 
-  it('returns -1 on weird input', async () => {
-    const response = { stdout: 'printer is offline or something/manually passing stuff', stderr: null };
+  it("returns -1 on weird input", async () => {
+    const response = {
+      stdout: "printer is offline or something/manually passing stuff",
+      stderr: null,
+    };
     const expected = null;
 
     expect(getRequestId(response)).toEqual(expected);
   });
 
-  it('returns -1 when response is empty', async () => {
-    const response = { stdout: '', stderr: null };
+  it("returns -1 when response is empty", async () => {
+    const response = { stdout: "", stderr: null };
     const expected = null;
 
     expect(getRequestId(response)).toEqual(expected);
   });
 
-  it('returns -1 when response is null', async () => {
+  it("returns -1 when response is null", async () => {
     const response = { stdout: null, stderr: null };
     const expected = null;
 
@@ -36,9 +42,11 @@ describe('getRequestId', () => {
   });
 });
 
-describe('isPrintComplete', () => {
+describe("isPrintComplete", () => {
   beforeEach(() => {
-    execAsync.mockImplementationOnce(() => Promise.resolve({ stdout: queuedStdout }));
+    execAsync.mockImplementationOnce(() =>
+      Promise.resolve({ stdout: queuedStdout })
+    );
   });
 
   afterEach(() => {
@@ -46,8 +54,11 @@ describe('isPrintComplete', () => {
     execAsync.mockRestore();
   });
 
-  it('job is still on the queue', async () => {
-    const printResponse = { stdout: 'request id is lp0-39 (1 file(s))', stderr: null };
+  it("job is still on the queue", async () => {
+    const printResponse = {
+      stdout: "request id is lp0-39 (1 file(s))",
+      stderr: null,
+    };
 
     const result = isPrintComplete(printResponse);
 
@@ -55,18 +66,24 @@ describe('isPrintComplete', () => {
     expect(execAsync).toBeCalledWith(`lpstat -o lp0`);
   });
 
-  it('job is not on the queue', async () => {
-    const printResponse = { stdout: 'request id is lp0-12 (1 file(s))', stderr: null };
+  it("job is not on the queue", async () => {
+    const printResponse = {
+      stdout: "request id is lp0-12 (1 file(s))",
+      stderr: null,
+    };
 
     const result = isPrintComplete(printResponse);
 
     await expect(result).resolves.toEqual(true);
   });
 
-  it('nothing on the queue', async () => {
-    const printResponse = { stdout: 'request id is lp0-39 (1 file(s))', stderr: null };
+  it("nothing on the queue", async () => {
+    const printResponse = {
+      stdout: "request id is lp0-39 (1 file(s))",
+      stderr: null,
+    };
     execAsync.mockRestore();
-    execAsync.mockImplementationOnce(() => Promise.resolve({ stdout: '' }));
+    execAsync.mockImplementationOnce(() => Promise.resolve({ stdout: "" }));
 
     const result = isPrintComplete(printResponse);
 
@@ -74,7 +91,10 @@ describe('isPrintComplete', () => {
   });
 
   it("getJobId didn't work", async () => {
-    const printResponse = { stdout: 'printer is offline or something/manually passing stuff', stderr: null };
+    const printResponse = {
+      stdout: "printer is offline or something/manually passing stuff",
+      stderr: null,
+    };
 
     const result = isPrintComplete(printResponse);
 
